6f669bc736d9612b02ef08bc8337ebf8a812bcde
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-10-13 17:35:11 -0500

    feat(auth.js prisma): added prisma integration

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import Auth0 from "next-auth/providers/auth0"
+import { prisma } from "@/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
 export const {  handlers, signIn, signOut, auth  } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Auth0({
     clientId: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
